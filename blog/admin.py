eac13045f6b5a4109b4af0f9aa82d853af436fb8
Erfan <yaghoblo41@gmail.com>
2023-06-26 19:44:33 +0330

    'V1'

---
+++ blog/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from .models import *  # all classes add to panel admin / add name class better than * but we use all class for *
+# Register your models here.
+from django_jalali.admin.filters import JDateFieldListFilter
+
+# you need import this for adding jalali calander widget
+import django_jalali.admin as jadmin
+
+# admin.sites.AdminSite
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'auther', 'publish', 'status']
+    ordering = ['title', 'publish']
+    list_filter = ['title', ('publish', JDateFieldListFilter), 'auther']
+    search_fields = ['title', 'description']
+    # raw_id_fields = ['auther']
+    date_hierarchy = 'publish'
+    prepopulated_fields = {"slug": ['title']}
+    list_editable = ['status']
+    # list_display_links = ['auther']
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['name', 'subject', 'phone']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'name', 'created', 'active']
+    list_filter = ['active', ('created', JDateFieldListFilter), ('update', JDateFieldListFilter)]
+    search_fields = ['name', 'body']
+    list_editable = ['active']
---
+++ blog/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django_jalali.db import models as jmodels
+from django.urls import reverse
+
+
+# Manager
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+# Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+        REJECTED = 'RJ', 'Rejected'
+
+    # relations
+    auther = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_posts")
+    # data fields
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    slug = models.SlugField(max_length=250)  # from urls
+    # data
+    publish = jmodels.jDateTimeField(default=timezone.now)  # from date
+    created = jmodels.jDateTimeField(auto_now_add=True)  # date now
+    update = jmodels.jDateTimeField(auto_now=True)  # change date if edit post
+    # choice fields
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    reading_time = models.PositiveIntegerField(verbose_name="زمان مطالعه")
+
+    # objects = models.Manager
+    objects = jmodels.jManager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
+
+
+class Ticket(models.Model):
+    message = models.TextField(verbose_name="پیام")
+    name = models.CharField(max_length=250, verbose_name="نام")
+    email = models.EmailField(verbose_name="ایمیل")
+    phone = models.CharField(max_length=11, verbose_name="شماره تماس")
+    subject = models.CharField(max_length=250, verbose_name="موضوع")
+
+    class Meta:
+        verbose_name = "تیکت"
+        verbose_name_plural = "تیکت ها"
+
+    def __str__(self):
+        return self.subject
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments", verbose_name="پست")
+    name = models.CharField(max_length=250, verbose_name="نام")
+    body = models.TextField(verbose_name="متن کامنت")
+    created = jmodels.jDateTimeField(auto_now_add=True, verbose_name="تاریخ ایجاد")  # date now
+    update = jmodels.jDateTimeField(auto_now=True, verbose_name="تاریخ ویرایش")  # change date if edit post
+    active = models.BooleanField(default=False, verbose_name="وضعیت")
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created'])
+        ]
+        verbose_name = "کامنت"
+        verbose_name_plural = "کامنت ها"
+
+    def __str__(self):
+        return f"{self.name}: {self.post}"
